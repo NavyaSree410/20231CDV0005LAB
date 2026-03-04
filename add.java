d97e8faffa5fe798d2f987ce976be97f64881057
Navya Sree <navyasree9490@gmail.com>
2026-03-04 12:14:11 +0530

    Rename class and implement even/odd check

--- add.java
+++ add.java
@@ -1,5 +1,12 @@
-public class add {
+public class EvenOddModulus {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // You can change this number to test different values
+        int number = 14; 
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
     }
 }
